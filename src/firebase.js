643284d7102893b85cfb525b37d9e6f64bcaca82
Bishesh Sunam <bisheshsunam@Bisheshs-MacBook-Pro.local>
2020-09-09 21:04:32 +1200

    'first commit, landing page'

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAl01GDE1bVOR10UJfZLb0_dcfFfvQHosw",
+  authDomain: "clone-57fec.firebaseapp.com",
+  databaseURL: "https://clone-57fec.firebaseio.com",
+  projectId: "clone-57fec",
+  storageBucket: "clone-57fec.appspot.com",
+  messagingSenderId: "93046023097",
+  appId: "1:93046023097:web:cb125b61727785e9c14c68",
+  measurementId: "G-J10YC0BB2H",
+};
