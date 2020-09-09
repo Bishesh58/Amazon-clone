0927edaffed7f1df8d216beb832a914754f73c9e
Bishesh Sunam <bisheshsunam@Bisheshs-MacBook-Pro.local>
2020-09-10 01:06:38 +1200

    added checkout modules

--- src/Home.js
+++ src/Home.js
@@ -5,63 +5,80 @@ import Product from "./Product";
 function Home() {
   return (
     <div className="home">
-      <div className="home_container"> 
-          {/* background image */}
-          <img className="home__image" 
-          src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" 
-          alt=""/>
-       
-       <div className="home__row">
+      <div className="home_container">
+        {/* background image */}
+        <img
+          className="home__image"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_2x._CB432458382_.jpg"
+          alt=""
+        />
+
+        <div className="home__row">
           <Product
-          id="12345"
-          title="The len startup"
-          price={29.99}
-          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
-          rating={5}
+            id="12345"
+            title="The len startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
           />
 
-         <Product
-          id="12345"
-          title="The len startup"
-          price={29.99}
-          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
-          rating={5}
+          <Product
+            id="12345"
+            title="The len startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
           />
-       </div>
 
-       <div className="home__row">
           <Product
-          id="12345"
-          title="The len startup"
-          price={29.99}
-          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
-          rating={5}
-          />
-          <Product
-          id="12345"
-          title="The len startup"
-          price={29.99}
-          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
-          rating={5}
+            id="12345"
+            title="The len startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
           />
           <Product
-          id="12345"
-          title="The len startup"
-          price={29.99}
-          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
-          rating={5}
+            id="12345"
+            title="The len startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
           />
-       </div>
-       
-       <div className="home__row">
-       <Product
-          id="12345"
-          title="The len startup"
-          price={29.99}
-          image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
-          rating={5}
+        </div>
+
+        <div className="home__row">
+          <Product
+            id="12345"
+            title="The len startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
           />
-       </div>
+          <Product
+            id="12345"
+            title="The len startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+          <Product
+            id="12345"
+            title="The len startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+        </div>
+
+        <div className="home__row">
+          <Product
+            id="12345"
+            title="The len startup"
+            price={29.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+            rating={5}
+          />
+        </div>
       </div>
     </div>
   );
