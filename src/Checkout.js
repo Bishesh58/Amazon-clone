d2e008c941365c19c822fd0c39fe81bcda9a7be6
Bishesh Sunam <bisheshsunam@Bisheshs-MacBook-Pro.local>
2020-09-10 17:11:48 +1200

    second commit, add to card and remove item functionality added

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from "./Subtotal";
+import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
+  const [{basket}, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -14,6 +18,17 @@ function Checkout() {
 
         <div>
           <h2 className="checkout__title">Your Shopping Basket</h2>
+
+
+          {basket.map(item => (
+            <CheckoutProduct 
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
           {/* Basket Item */}
           {/* Basket Item */}
           {/* Basket Item */}
